ecfec29086dc761d0cd0fe3ea5227836bfa4fb37
ronkeizer <ronkeizer@gmail.com>
2015-03-10 14:08:38 -0700

    fix small bug in model

--- R/pk_1cmt_oral.R
+++ R/pk_1cmt_oral.R
@@ -5,7 +5,7 @@ pk_1cmt_oral <- function (t, A, p) {
   with(p, {
     KEL <-  CL/V
     dAdt_1 <- -KA*A[1]
-    dAdt_2 <- -KEL*A[2] + KA*A(1)
+    dAdt_2 <- -KEL*A[2] + KA*A[1]
     return ( list ( c (  dAdt_1, dAdt_2 ) ) )
   })
 }
