1e0e74308b2965937199a1be5085279f705f3cf5
ronkeizer <ronkeizer@gmail.com>
2015-03-12 14:08:46 -0700

    add folder

---
+++ inst/server.R
@@ -0,0 +1,75 @@
+library(shiny)
+library(deSolve)
+library(dplyr)
+library(ggplot2)
+
+ode     <- readRDS("ode.rds")
+p       <- readRDS("parameters.rds")
+len     <- round(length(names(p))/2)
+regimen <- readRDS("regimen.rds")
+misc    <- readRDS("misc.rds")
+if(is.null(misc$tmax)) {
+  if(!is.null(regimen$interval)) {
+    misc$tmax <- (regimen$interval * (regimen$n+1))
+  } else {
+    misc$tmax <- max(regimen$times)
+  }
+}
+shinyServer(function(input, output) {
+  output$parInputs <-
+    renderUI({
+      w <- h4("Model parameters")
+      for(j in 1:len) {
+        idx <- (j-1)*2+1
+        if (j < len || len == length(names(p))/2) {
+          val1 <- p[[names(p)[idx]]]
+          val2 <- p[[names(p)[idx+1]]]
+          w <- paste(w, fluidRow(column(6, sliderInput(names(p)[idx], names(p)[idx], min=signif(val1/5, 1), max=signif(val1*3,1), value=val1)),
+                                 column(6, sliderInput(names(p)[idx+1], names(p)[idx+1], min=signif(val2/5, 1), max=signif(val2*3,1), value=val2)
+                                 )))
+        } else {
+          val1 <- p[[names(p)[idx]]]
+          w <- paste(w, fluidRow(column(6, sliderInput(names(p)[idx], names(p)[idx], min=signif(val1/5, 1), max=signif(val1*3,1), value=val1)
+          )))
+        }
+      }
+      HTML(w)
+    })
+  output$ind_plot <- renderPlot({
+    regimen <- new_regimen(
+      amt = as.numeric(input$amt),
+      n = input$n,
+      interval = as.numeric(input$interval),
+      type = tolower(input$type),
+      t_inf = as.numeric(input$t_inf)
+    )
+    pars <- p
+    for(i in 1:length(names(p))) {
+      if (!is.null(input[[names(p)[i]]])) {
+        pars[[names(p)[i]]] <- input[[names(p)[i]]]
+      }
+    }
+    dat <- sim_ode (ode = ode,
+                    parameters = pars,
+                    omega = misc$omega,
+                    n_ind = input$n_ind,
+                    regimen = regimen,
+                    A_init = misc$A_init,
+                    adherence = list(type = "markov", markov = list(p01 = input$adh_p01, p11 = input$adh_p11)),
+                    tmax = NULL)
+    if (input$plot_show != "all compartments") {
+      dat <- dat %>% filter(comp == "obs")
+    }
+    p <- ggplot(dat, aes(x=t, y=y, colour=factor(id), group=id)) +
+      geom_line() +
+      facet_grid(comp ~ .) +
+      theme_plain() +
+      scale_colour_discrete(guide = FALSE) +
+      xlab("time") + ylab("")
+    if(input$plot_yaxis == "log10") {
+      p <- p + scale_y_log10()
+    }
+    return(p)
+  })
+  outputOptions(output, name = "parInputs")
+})
---
+++ inst/ui.R
@@ -0,0 +1,49 @@
+library(shiny)
+
+parInputs <- function(idx) {
+  wellPanel(
+    htmlOutput("parInputs")
+  )
+}
+
+# Define UI for application that draws a histogram
+fluidPage(
+  fluidRow(p(" ")),
+  fluidRow(
+    column(3,
+           wellPanel(
+             h4("Regimen"),
+             fluidRow(
+               column(12, sliderInput("n_ind", "Number of individuals:", min = 1, max = 50, value = 1))),
+             fluidRow(
+               column(6, textInput("amt", "Amount:", value = "100")),
+               column(6, textInput("interval", "Interval:", value = "12"))),
+             fluidRow(
+               column(12, sliderInput("n", "Number of doses:", min = 1, max = 20, value = 3),
+                      selectInput("type", "Dose type", c("Bolus", "Infusion"), selected="Bolus"),
+                      sliderInput("t_inf", "Infusion length:", min = 1, max = 12, value = 2)
+               )
+             )
+           ),
+           wellPanel(
+             h4("Adherence"),
+             fluidRow(
+               column(6, sliderInput("adh_p11", "p(1 -> 1)", min=0, max=1, value=1)),
+               column(6, sliderInput("adh_p01", "p(0 -> 1)", min=0, max=1, value=1))
+             )
+           )
+    ),
+    column(6,
+           plotOutput("ind_plot"),
+           wellPanel(
+             h4("Plot"),
+             fluidRow(
+               column(6, selectInput("plot_show", "Show:", c("all compartments", "observations only"), selected="all compartments")),
+               column(4, selectInput("plot_yaxis", "Y-axis:", c("log10", "untransformed"), selected="log10"))
+             )
+           )
+    ),
+    column(3, parInputs("a"))
+  ),
+  theme = "style.css"
+)
