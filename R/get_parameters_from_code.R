397c546c5144068ba2df10e41a2b23d57053cecc
ronkeizer <ronkeizer@gmail.com>
2015-04-29 14:45:20 -0700

    update get_parameters_from_code

--- R/get_parameters_from_code.R
+++ R/get_parameters_from_code.R
@@ -1,9 +1,17 @@
 #' @export
 get_parameters_from_code <- function (code, declare_variables = NULL) {
   code <- gsub("\\n", "", code)
+
+  def <- gregexpr("double (.*?)=", code)
+  def1 <- unlist(regmatches(code, def))
+  defined <- gsub("(double|=|\\s)", "", def1)
+  print(defined)
+  code <- gsub("double (.*?)=", "", code) # parameter delcarations in code
+  print(code)
+  code <- gsub("double (.*?)=", "", code) # parameter delcarations in code
   code <- gsub("[\\;\\/\\*\\^\\+\\=\\(\\)\\-\\{\\}\\>\\<]", " ", code)
   code <- gsub("\\-", " ", code)
-  code <- gsub("(if|then|else|double)", " ", code)
+  code <- gsub("(if|then|else)", " ", code)
   A <- gregexpr("A\\[([0-9])\\]", code)
   m1 <- paste0(unlist(regmatches(code, A, invert = TRUE)), collapse="")
   dAdt <- gregexpr("dAdt\\[([0-9])\\]", m1)
@@ -17,11 +25,16 @@ get_parameters_from_code <- function (code, declare_variables = NULL) {
     }
   }
   pars <- c(unique(spl2))
+
   reserved_words <- c("beta", "gamma") # add more later
   if (any(reserved_words %in% pars)) {
     stop("The following variable names are reserved words and cannot be used in your code, please rename:\n   ", paste(pars[match(reserved_words, pars)], collapse=" "))
   } else {
+    defined <- c(defined, "t")
+    match_def <- match(defined, pars)
+    if (length(match_def) > 0) {
+      pars <- pars[-match_def[!is.na(match_def)]]
+    }
     return(pars)
   }
 }
-
