15946f1612fd458289a2186228b36f1e4b628443
ronkeizer <ronkeizer@gmail.com>
2015-04-30 12:08:48 -0700

    add file

---
+++ R/test_pointer.R
@@ -0,0 +1,7 @@
+#' @export
+test_pointer <- function(model) {
+  tmp <- capture.output(model)
+  if(length(grep("0x0>", tmp[2])) > 0) {
+    stop("Model not in memory anymore. Please recompile using new_ode_model().")
+  }
+}
