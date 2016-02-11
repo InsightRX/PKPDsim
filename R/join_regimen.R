0566079d78bad68a658e3701eac92126ca550cbe
ronkeizer <ronkeizer@gmail.com>
2016-02-11 09:53:55 -0800

    updates

--- R/join_regimen.R
+++ R/join_regimen.R
@@ -8,14 +8,31 @@
 join_regimen <- function(
   regimen1 = NULL, regimen2 = NULL,
   interval = NULL, dose_update = NULL) {
-  if(!is.null(regimen1) && !is.null(regimen2)) {
+  if(is.null(regimen1) && is.null(regimen2)) {
+    stop("please specify two intervals to this function.")
+  } else {
+    if(is.null(regimen1)) { # if one of the intervals is NULL, just return the other
+      return(regimen2)
+    }
+    if(is.null(regimen2)) {
+      return(regimen1)
+    }
     if(is.null(interval) && is.null(dose_update)) {
       stop("either interval or dose_update have to be specified as arguments")
     }
     if(!is.null(dose_update)) {
-      t <- c(regimen1$dose_times[1:(dose_update-1)], regimen2$dose_times + regimen1$dose_times[dose_update])
-      amt <- c(regimen1$dose_amts[1:(dose_update-1)], regimen2$dose_amts)
-      t_inf <- c(regimen1$t_inf[1:(dose_update-1)], regimen2$t_inf)
+      if(dose_update < 1) {
+        stop("dose_update argument should be >= 1.")
+      }
+      if(dose_update > 1) {
+        t <- c(regimen1$dose_times[1:(dose_update-1)], regimen2$dose_times + regimen1$dose_times[dose_update])
+        amt <- c(regimen1$dose_amts[1:(dose_update-1)], regimen2$dose_amts)
+        t_inf <- c(regimen1$t_inf[1:(dose_update-1)], regimen2$t_inf)
+      } else {
+        t <- c(regimen2$dose_times + regimen1$dose_times[dose_update])
+        amt <- c(regimen2$dose_amts)
+        t_inf <- c(regimen2$t_inf)
+      }
       joint <- new_regimen(amt = amt, times = t, t_inf = t_inf, type = regimen1$type)
       return(joint)
     }
