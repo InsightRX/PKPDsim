4fd568e97ed8a7c588d4e3538e3c9193a47deb91
ronkeizer <ronkeizer@gmail.com>
2016-02-11 09:40:36 -0800

    add join_regimen funciton

---
+++ R/join_regimen.R
@@ -0,0 +1,32 @@
+#' Join two dosing regimens
+#'
+#' @param regimen1 first regimen
+#' @param regimen2 second regimen
+#' @param interval interval between regimen1 and regimen2 (if dose_update not specified)
+#' @param dose_update dose number at which to override regimen1 with regimen 2 (if interval not specified)
+#' @export
+join_regimen <- function(
+  regimen1 = NULL, regimen2 = NULL,
+  interval = NULL, dose_update = NULL) {
+  if(!is.null(regimen1) && !is.null(regimen2)) {
+    if(is.null(interval) && is.null(dose_update)) {
+      stop("either interval or dose_update have to be specified as arguments")
+    }
+    if(!is.null(dose_update)) {
+      t <- c(regimen1$dose_times[1:(dose_update-1)], regimen2$dose_times + regimen1$dose_times[dose_update])
+      amt <- c(regimen1$dose_amts[1:(dose_update-1)], regimen2$dose_amts)
+      t_inf <- c(regimen1$t_inf[1:(dose_update-1)], regimen2$t_inf)
+      joint <- new_regimen(amt = amt, times = t, t_inf = t_inf, type = regimen1$type)
+      return(joint)
+    }
+    if(!is.null(interval)) {
+      joint <- new_regimen(
+        amt = c(regimen1$dose_amts, regimen2$dose_amts),
+        times = c(regimen1$dose_times, regimen2$dose_times + tail(regimen1$dose_times,1) + interval),
+        type = "infusion",
+        t_inf = c(regimen1$t_inf, regimen2$t_inf)
+      )
+      return(joint)
+    }
+  }
+}
