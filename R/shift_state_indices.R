8ee23767ce6d212bf51922c7490f829df199ce26
ronkeizer <ronkeizer@gmail.com>
2016-04-20 13:07:29 -0700

    fix bug shifting state indices

--- R/shift_state_indices.R
+++ R/shift_state_indices.R
@@ -7,11 +7,16 @@
 shift_state_indices <- function (ode_def, n = -1) {
   m <- gregexpr("\\[([0-9])\\]", ode_def)
   m1 <- unlist(regmatches(ode_def, m))
-  if(length(m1) > 0 && min(m1) > 0) {
-    m2 <- unlist(regmatches(ode_def, m, invert = TRUE))
+  if(length(m1) > 0) {
     for (i in seq(m1)) {
-      tmp <- as.numeric(gsub("[^0-9]", "", m1[i]))
-      m1[i] <- paste0("[", tmp + n, "]")
+      m1[i] <- as.numeric(gsub("[^0-9]", "", m1[i]))
+    }
+    m2 <- unlist(regmatches(ode_def, m, invert = TRUE))
+    if(min(m1) > 0) {
+      for (i in seq(m1)) {
+        tmp <- as.numeric(gsub("[^0-9]", "", m1[i]))
+        m1[i] <- paste0("[", tmp + n, "]")
+      }
     }
     return(paste0(c(m2[1], t(cbind(m1,m2[-1]))), collapse=""))
   } else {
