85f40f32955d5556b18101a9c4b2baa7298a0dc6
ronkeizer <ronkeizer@gmail.com>
2016-06-27 10:13:05 -0700

    small fix join regimen function

--- R/join_regimen.R
+++ R/join_regimen.R
@@ -6,8 +6,10 @@
 #' @param dose_update dose number at which to override regimen1 with regimen 2 (if interval not specified)
 #' @export
 join_regimen <- function(
-  regimen1 = NULL, regimen2 = NULL,
-  interval = NULL, dose_update = NULL) {
+  regimen1 = NULL,
+  regimen2 = NULL,
+  interval = NULL,
+  dose_update = NULL) {
   if(is.null(regimen1) && is.null(regimen2)) {
     stop("please specify two intervals to this function.")
   } else {
@@ -26,7 +28,7 @@ join_regimen <- function(
     if(!is.null(dose_update)) {
       if(dose_update > 1) {
         if(dose_update <= length(regimen1$dose_times)) {
-          t <- c(regimen1$dose_times[1:(dose_update-1)], regimen2$dose_times + regimen1$dose_times[dose_update])
+          t <- c(regimen1$dose_times[1:(dose_update-1)], regimen2$dose_times + tail(regimen1$dose_times,1))
           amt <- c(regimen1$dose_amts[1:(dose_update-1)], regimen2$dose_amts)
           t_inf <- c(regimen1$t_inf[1:(dose_update-1)], regimen2$t_inf)
         } else { # just add to regimen
