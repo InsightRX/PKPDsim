945b28b85c3a355371e0f15d94a2e92d27aaafa4
ronkeizer <ronkeizer@gmail.com>
2017-06-26 09:46:59 +0200

    small fix print regimen

--- R/print.regimen.R
+++ R/print.regimen.R
@@ -4,12 +4,14 @@
 #' @param ... arguments to pass
 #' @export
 print.regimen <- function(x, ...) {
-  tmp <- data.frame(cbind(t = x$dose_times, type = x$type, amt = x$dose_amts, cmt = x$cmt))
-  if(!is.null(x$t_inf)) {
-    tmp <- data.frame(cbind(tmp, t_inf = x$t_inf))
+  if(!is.null(x) && !is.null(x$dose_times) && length(x$dose_times) > 0) {
+    tmp <- data.frame(cbind(t = x$dose_times, type = x$type, amt = x$dose_amts, cmt = x$cmt))
+    if(!is.null(x$t_inf)) {
+      tmp <- data.frame(cbind(tmp, t_inf = x$t_inf))
+    }
+    if(!is.null(x$ss_regimen)) {
+      message("Note: Steady state regimen, steady state assumed to be reached at t = 0.\n")
+    }
+    print(tmp)
   }
-  if(!is.null(x$ss_regimen)) {
-    cat("Note: Steady state regimen, steady state assumed to be reached at t = 0.\n")
-  }
-  print(tmp)
 }
