f7a64e3c4710003741ae7d05f92cd40b0dbffb86
ronkeizer <ronkeizer@gmail.com>
2017-08-16 13:13:38 -0700

    remove msg

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,1 @@
-message("\n----\nWarning: \n\nDevelopment of the PKPDsim package has moved from ronkeizer/PKPDsim to InsightRX/PKPDsim, please get the latest version from the new repository.\n----\n")
+# message("\n----\nWarning: \n\nDevelopment of the PKPDsim package has moved from ronkeizer/PKPDsim to InsightRX/PKPDsim, please get the latest version from the new repository.\n----\n")
