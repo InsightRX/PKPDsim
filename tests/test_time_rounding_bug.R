8e046b8601b928886779d110092fb048b2bfd27a
Ron Keizer <ron@insight-rx.com>
2019-10-01 15:07:04 -0700

    fix more tests

--- tests/test_time_rounding_bug.R
+++ tests/test_time_rounding_bug.R
@@ -12,5 +12,5 @@ r1 <- new_regimen(amt = 100,
                   times = c(0, 24, 36),
                   type = "infusion")
 mod <- new_ode_model(model = "pk_2cmt_iv")
-dat1 <- sim_ode (ode = mod, par = p, regimen = r1, t_obs=seq(from=0, to=150, len=100))
+dat1 <- sim_ode (ode = mod, parameters = p, regimen = r1, t_obs=seq(from=0, to=150, len=100))
 testit::assert("Should have no NAs", !any(is.na(dat1$y)))
