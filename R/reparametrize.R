cff2b8475b5a42548c669d30e7085380874bda8f
Ron Keizer <ron@insight-rx.com>
2020-08-05 16:47:59 -0700

    add covariates parser to reparametrize func; add docs

--- R/reparametrize.R
+++ R/reparametrize.R
@@ -3,15 +3,19 @@
 #' Mostly useful for reparametrizing models into standard parametrizations, e.g. to
 #' NONMEM TRANS or clinPK parametrizations.
 #'
-#' @param parameters list of model parameters
 #' @param model PKPDsim model, compiled using `reparametrization` argument or in metadata object.
+#' @param parameters list of model parameters
+#' @param covariates covariates list, specified as PKPDsim covariates
 #'
 #' @export
-reparametrize <- function(parameters, model) {
+reparametrize <- function(model, parameters, covariates) {
   reparametrization <- attr(model, "reparametrization")
   if(is.null(reparametrization)) {
     warning("No reparametrization found, returning original parameters object.")
     return(parameters)
+  }
+  if(!is.null(covariates)) {
+    parameters <- c(parameters, lapply(covariates, function(x) { tail(x$value) } ))
   }
   with(parameters, {
     repar <- list()
