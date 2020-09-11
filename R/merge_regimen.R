d388f2da5ad5c070d9f70c9072e1042b06c0154b
Ron Keizer <ron@insight-rx.com>
2020-09-11 11:13:22 -0700

    minor fixes

--- R/merge_regimen.R
+++ R/merge_regimen.R
@@ -7,14 +7,14 @@
 #'
 #' @export
 merge_regimen <- function(regimens) {
-  reg <- data.frame(regimens[[1]])
-  reg$type <- as.character(reg$type)
+  cols <- c("interval", "n", "type", "t_inf", "dose_times", "dose_amts", "first_dose_time")
+  reg <- data.frame(regimens[[1]], stringsAsFactors = FALSE)[, cols]
   if(length(regimens) > 1) {
     for(i in 2:length(regimens)) {
       reg_tmp <- data.frame(regimens[[i]])
       reg_tmp$type <- as.character(reg_tmp$type)
       reg_tmp$t_inf <- ifelse0(0, reg_tmp$t_inf)
-      reg <- rbind(reg, reg_tmp[,c("interval", "n", "type", "t_inf", "dose_times", "dose_amts", "first_dose_time")])
+      reg <- rbind(reg, reg_tmp[, cols])
     }
   }
   reg <- reg[order(reg$dose_times, decreasing = FALSE),]
