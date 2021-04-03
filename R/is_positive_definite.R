a146fb5b9c720d3d1753223d8493b1b6ec724967
jasmineirx <jasmine@insight-rx.com>
2021-04-03 01:44:17 +0000

    complex numbers discarded if tiny

--- R/is_positive_definite.R
+++ R/is_positive_definite.R
@@ -4,7 +4,13 @@
 #' @export
 is_positive_definite <- function(x) {
   if(! "matrix" %in% class(x)) x <- triangle_to_full(x)
-  eig <- eigen(x)
+  eig <- eigen(x, only.values = TRUE)
+  if (any(is.complex(eig$values))) {
+    if (any(Im(eig$values) >= 1e-15)) {
+      return(FALSE)
+    }
+    eig$values <- as.numeric(eig$values)
+  }
   if(any(eig$values <= 0)) {
     return(FALSE)
   } else {
