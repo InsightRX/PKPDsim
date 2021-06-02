5dec059d252828caa862b8a1608de337c4805dce
jasmineirx <jasmine@insight-rx.com>
2021-06-02 11:24:56 -0700

    moved tests from one file to another similar file

--- tests/testthat/test_multi_obs.R
+++ tests/testthat/test_multi_obs.R
@@ -89,3 +89,53 @@ test_that("check that residual error correctly applied to right var", {
   expect_equal(data3$y[-c(1,3,5)], y[-c(1,3,5)])
   expect_true(all(data3$y[c(1,3,5)] != y[c(1,3,5)]))
 })
+
+
+test_that("specifying ruv as multi-type when only 1 obs_type", {
+  ruv_single <- list(prop = 0.1, add = 1)
+  ruv_multi <- list(prop = c(0.1, 1), add = c(1, 20))
+
+  s_single <- sim(
+    ode = pk1,
+    parameters = parameters,
+    n_ind = 1,
+    regimen = regimen,
+    only_obs = TRUE,
+    t_obs = c(2,4,6,8),
+    seed = 123456,
+    res_var = ruv_single
+  )
+
+  ## specified as multi, but obs_type is all 1, so should
+  ## give same results as s_single
+  s_multi1 <- sim(
+    ode = pk1,
+    parameters = parameters,
+    n_ind = 1,
+    regimen = regimen,
+    only_obs = TRUE,
+    t_obs = c(2,4,6,8),
+    obs_type = c(1, 1, 1, 1),
+    seed = 123456,
+    res_var = ruv_multi,
+    t_init = 10
+  )
+  expect_equal(s_multi1$y, s_single$y)
+
+  ## specifying ruv as multi-type when multiple obs_type
+  ## should now give different results
+  s_multi1 <- sim(
+    ode = pk1,
+    parameters = parameters,
+    n_ind = 1,
+    regimen = regimen,
+    only_obs = TRUE,
+    t_obs = c(2,4,6,8),
+    obs_type = c(1, 2, 1, 2),
+    seed = 123456,
+    res_var = ruv_multi,
+    t_init = 10
+  )
+  expect_equal(s_multi1$y[c(1, 3)], s_single$y[c(1,3)])
+  expect_true(sum(abs(s_single$y[c(2,4)] - s_multi1$y[c(2,4)])) > 50)
+})
