fce6dfb9e99e6aa7d788d3eb3073524eee563848
jasmineirx <jasmine@insight-rx.com>
2021-06-07 17:22:05 -0700

    test install from api

---
+++ tests/testthat/test_model_from_api.R
@@ -0,0 +1,36 @@
+test_that("Can specify a package from json", {
+  mod <- model_from_api(
+    model = "test_1cmt_iv",
+    url = system.file(package = "PKPDsim"),
+    to_package = FALSE,
+    verbose = FALSE,
+    force = TRUE
+  )
+  expect_true("PKPDsim" %in% class(mod))
+})
+
+test_that("Can install a package from json", {
+  if ("test1cmtiv" %in% installed.packages()){
+    suppressMessages(remove.packages("test1cmtiv"))
+  }
+  suppressWarnings(
+    suppressMessages(
+      mod <- model_from_api(
+        model = "test_1cmt_iv",
+        url = system.file(package = "PKPDsim"),
+        to_package = TRUE,
+        verbose = FALSE,
+        force = TRUE
+      )
+    )
+  )
+  expect_true("test1cmtiv" %in% installed.packages())
+  suppressMessages(require(test1cmtiv))
+  mod_from_pkg <- get("model", asNamespace("test1cmtiv"))()
+  expect_true("PKPDsim" %in% class(mod_from_pkg))
+  if ("test1cmtiv" %in% installed.packages()){
+    suppressMessages(remove.packages("test1cmtiv"))
+  }
+})
+
+
