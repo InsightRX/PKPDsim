1379913956fd16d0df3ed5907b403b8b0ea88df8
jasmineirx <jasmine@insight-rx.com>
2021-06-08 09:52:33 -0700

    test model_from_api

---
+++ inst/tests/test_1cmt_iv.R
@@ -0,0 +1,5 @@
+library(testthat)
+
+test_that("Model tests run", {
+  expect_true(TRUE)
+})
--- tests/testthat/test_model_from_api.R
+++ tests/testthat/test_model_from_api.R
@@ -9,7 +9,7 @@ test_that("Can specify a package from json", {
   expect_true("PKPDsim" %in% class(mod))
 })
 
-test_that("Can install a package from json", {
+test_that("Can install and test a package from json", {
   if ("test1cmtiv" %in% installed.packages()){
     suppressMessages(remove.packages("test1cmtiv"))
   }
@@ -20,10 +20,11 @@ test_that("Can install a package from json", {
         url = system.file(package = "PKPDsim"),
         to_package = TRUE,
         verbose = FALSE,
-        force = TRUE
+        run_tests = TRUE
       )
     )
   )
+  # model_from_api will stop if test fails/can't be found
   expect_true("test1cmtiv" %in% installed.packages())
   suppressMessages(require(test1cmtiv))
   mod_from_pkg <- get("model", asNamespace("test1cmtiv"))()
