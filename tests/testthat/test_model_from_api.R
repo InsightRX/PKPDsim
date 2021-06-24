e838ce80508c8c6f7552b07834ec5eec5a6b79db
jasmineirx <jasmine@insight-rx.com>
2021-06-24 22:07:58 +0000

    specify location

--- tests/testthat/test_model_from_api.R
+++ tests/testthat/test_model_from_api.R
@@ -10,14 +10,18 @@ test_that("Can specify a package from json", {
 })
 
 test_that("Can install and test a package from json", {
-  if ("test1cmtiv" %in% installed.packages()){
-    suppressMessages(remove.packages("test1cmtiv"))
+  curr_dir <- Sys.getenv("TMPDIR")
+  dir.create("/tmp/testpk")
+  Sys.setenv(TMPDIR = "/tmp/testpk")
+  if ("test1cmtiv" %in% installed.packages(lib.loc = c("/tmp/testpk", ""))){
+    suppressMessages(remove.packages("test1cmtiv", lib.loc = c("/tmp/testpk", "")))
   }
   suppressWarnings(
     suppressMessages(
       mod <- model_from_api(
         model = "test_1cmt_iv",
         url = system.file(package = "PKPDsim"),
+        lib_location = "/tmp/testpk",
         to_package = TRUE,
         verbose = FALSE,
         run_tests = TRUE
@@ -25,13 +29,14 @@ test_that("Can install and test a package from json", {
     )
   )
   # model_from_api will stop if test fails/can't be found
-  expect_true("test1cmtiv" %in% installed.packages())
-  suppressMessages(require(test1cmtiv))
+  expect_true("test1cmtiv" %in% installed.packages(lib.loc = "/tmp/testpk"))
+  suppressMessages(require(test1cmtiv, lib.loc = "/tmp/testpk"))
   mod_from_pkg <- get("model", asNamespace("test1cmtiv"))()
   expect_true("PKPDsim" %in% class(mod_from_pkg))
-  if ("test1cmtiv" %in% installed.packages()){
-    suppressMessages(remove.packages("test1cmtiv"))
+  if ("test1cmtiv" %in% installed.packages(lib.loc = c("/tmp/testpk", ""))){
+    suppressMessages(remove.packages("test1cmtiv", lib = c("", "/tmp/testpk")))
   }
+  Sys.setenv(TMPDIR = curr_dir)
 })
 
 
