1d9af9d8ab87c74ab0f76310e6b759c31f0402b2
Kara Woo <kara@insight-rx.com>
2021-12-20 15:07:11 -0800

    Remove commented message

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,3 @@
-# message("\n----\nWarning: \n\nDevelopment of the PKPDsim package has moved from InsightRX/PKPDsim to InsightRX/PKPDsim, please get the latest version from the new repository.\n----\n")
-
 #' @useDynLib PKPDsim
 #' @importFrom Rcpp sourceCpp
 NULL
