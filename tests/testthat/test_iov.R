66f629314c2e4c482b7d07703abeb4ad69bb1969
Ron Keizer <ron@insight-rx.com>
2022-02-03 19:48:40 +0000

    update unit test

--- tests/testthat/test_iov.R
+++ tests/testthat/test_iov.R
@@ -71,3 +71,62 @@ test_that("IOV is added to parameters", {
   )
 })
 
+test_that("Change in F in 2nd bin is applied in 2nd bin and not later.", {
+  # Previously this was an issue because F, when defined in pk_code(), was not updated before the
+  # dose was applied to the state vector, so the bioavailability was not applied at the right time.
+  # This was fixed by rearranging the order of execution in sim.cpp in the main loop.
+
+  pars <- list(
+    "CL" = 5,
+    "V" = 50,
+    "KA" = 1,
+    "F" = 1
+  )
+  pk1 <- new_ode_model(
+    code = "
+        dAdt[1] = -KA * A[1]
+        dAdt[2] = +KA * A[1] -(CLi/V) * A[2]
+    ",
+    pk_code = "
+        Fi = F * exp(kappa_F);
+        CLi = CL;
+    ",
+    iov = list(
+      cv = list(F = 0.2),
+      n_bins = 4
+    ),
+    obs = list(cmt = 2, scale = "V"),
+    dose = list(cmt = 1, bioav = "Fi"),
+    declare_variables = c("kappa_F", "Fi", "CLi"),
+    parameters = names(pars),
+    cpp_show_code = F
+  )
+  reg <- new_regimen(amt = 800, interval = 24, n = 10, type = "oral")
+
+  # For a first simulation, we're simulating with no variability across the IOV bins:
+  pars$kappa_F_1 <- 0
+  pars$kappa_F_2 <- 0
+  pars$kappa_F_3 <- 0
+  args_sim1 <- args <- list(
+    ode = pk1,
+    parameters = pars,
+    regimen = reg,
+    only_obs = TRUE,
+    t_obs = seq(0, 50, .25),
+    iov_bins = c(0L, 24L, 48L, 9999L)
+  )
+  # For a second simulation, we're applying a change in parameter for the 2nd bin (24-48 hrs).
+  # This should affect predictions from 24 onward.
+  pars$kappa_F_2 <- 1 # 2nd bin
+  args_sim2 <- args <- list(
+    ode = pk1,
+    parameters = pars,
+    regimen = reg,
+    only_obs = TRUE,
+    t_obs = seq(0, 50, .25),
+    iov_bins = c(0L, 24L, 48L, 9999L)
+  )
+  res1 <- do.call("sim_ode", args = args_sim1)
+  res2 <- do.call("sim_ode", args = args_sim2)
+  expect_true(min(res1[res1$y != res2$y,]$t) <= 25)
+})
