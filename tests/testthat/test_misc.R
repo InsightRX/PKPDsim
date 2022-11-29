3ba2926bb60b7a818967470684363971bb99d75c
Kara Woo <kara@insight-rx.com>
2022-11-29 12:58:23 -0800

    Rewrite print_list

--- tests/testthat/test_misc.R
+++ tests/testthat/test_misc.R
@@ -8,8 +8,29 @@ test_that("now_utc returns time in UTC", {
   expect_equal(attr(now, "tzone"), "UTC")
 })
 
+test_that("print_list returns expected results", {
+  l1 <- list(x = 1, y = 2)
+  l2 <- list(x = 1, y = "foo")
+  l3 <- list(x = list(y = "foo"), z = "bar")
+  res1 <- print_list(l1)
+  res2 <- print_list(l2)
+  res3 <- print_list(l3)
+  expect_equal(res1, "list(x = 1, y = 2)")
+  expect_equal(res2, "list(x = 1, y = \"foo\")")
+  expect_equal(res3, "list(x = list(y = \"foo\"), z = \"bar\")")
+})
+
+test_that("print_list can remove outer list() call", {
+  l <- list(x = 1, y = 2)
+  expect_equal(print_list(l, wrapper = FALSE), "x = 1, y = 2")
+})
+
 test_that("print_list supports empty lists", {
   x <- list()
   res <- print_list(x)
   expect_equal(res, "")
+})
+
+test_that("print_list supports NULL input", {
+  expect_equal(print_list(NULL), "")
 })
