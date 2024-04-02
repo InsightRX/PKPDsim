86f24137c1a8059c0ac0756df495a990c3067429
Ron Keizer <ron@insight-rx.com>
2024-04-02 09:41:46 -0700

    rm lines not needed; add test

---
+++ tests/testthat/test_apply_duration_scale.R
@@ -0,0 +1,49 @@
+reg_oral <- new_regimen(
+  amt = 1000,
+  n = 12,
+  interval = 12,
+  type = "oral"
+)
+reg_iv <- new_regimen(
+  amt = 1000,
+  n = 12,
+  interval = 12,
+  type = "infusion",
+  t_inf = 1
+)
+
+## null / error checks
+test_that("infusion length NULL warning", {
+  expect_warning(
+    dur0 <- apply_duration_scale(
+      regimen = reg_oral
+    )
+  )
+  expect_equal(dur0, reg_oral)
+})
+test_that("duration scale NULL warning", {
+  expect_warning(
+    dur1 <- apply_duration_scale(
+      regimen = reg_iv
+    )
+  )
+  expect_equal(dur1, reg_iv)
+})
+
+## Check functionality
+test_that("infusion length scaled", {
+  dur2 <- apply_duration_scale(
+    regimen = reg_iv,
+    duration_scale = 1.5
+  )
+  expect_equal(dur2$t_inf, rep(1.5, 12))
+})
+
+test_that("infusion length scaled using parameter", {
+  dur3 <- apply_duration_scale(
+    regimen = reg_iv,
+    duration_scale = "SCALE",
+    parameters = list(CL = 5, V = 50, SCALE = 1.6)
+  )
+  expect_equal(dur3$t_inf, rep(1.6, 12))
+})
