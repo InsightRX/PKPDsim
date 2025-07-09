dafa5c3dcd25b92fac60be8fa606939410c9613d
roninsightrx <ron@insight-rx.com>
2025-07-09 04:03:48 -0700

    Update R/parse_lagtime.R
    Co-authored-by: Jasmine Hughes <43552465+jasmineirx@users.noreply.github.com>

--- R/parse_lagtime.R
+++ R/parse_lagtime.R
@@ -8,13 +8,13 @@ parse_lagtime <- function(
   lagtime,
   ode
 ) {
-  if(!is.null(attr(ode, "lagtime")) && attr(ode, "lagtime")[1] != "undefined" && attr(ode, "lagtime")[1] != "NULL") {
-    if(is.null(lagtime)) { # only override from metadata if not specified by user
-      lagtime <- attr(ode, "lagtime")
-    }
+  lagtime_ode <- attr(ode, "lagtime")
+  # override from ode if not specified by user and defined in ode
+  if(is.null(lagtime) && !is.null(lagtime_ode) && !lagtime_ode %in% c("NULL", "undefined")) {
+    lagtime <- lagtime_ode
   }
   if(is.null(lagtime)) {
-    lagtime <- c(0)
+    lagtime <- 0
   }
   lagtime
 }
